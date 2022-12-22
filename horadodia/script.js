786955c0bcf6cd6cd0645af709978dfffe1bbaeb
mariafcknt <105084673+mariafcknt@users.noreply.github.com>
2022-12-22 14:26:07 -0300

    Update script.js

--- horadodia/script.js
+++ horadodia/script.js
@@ -3,12 +3,12 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    var minuto = data.getMinutes()
+    //var minuto = data.getMinutes()
     var link = window.document.getElementById('link')
     link.addEventListener('mouseenter',entrar)
     link.addEventListener('mouseout',sair)
 
-    msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos`
+    msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
         document.body.style.background = '#6D7A4C'
